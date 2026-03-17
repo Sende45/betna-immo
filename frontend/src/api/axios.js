fcd752ed1fdaac574eb15097d519121f20941b64
Sende45 <ton-email@gmail.com>
2026-03-17 01:53:02 +0100

    ajout des nouvelles mises à jour

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // ✅ Utilise la variable d'env Vercel en priorité, sinon localhost pour ton PC
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 
 // --- L'INTERCEPTEUR : La magie automatique ---
