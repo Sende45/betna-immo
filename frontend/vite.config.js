7a5f51b499df313252890622ef1993d639ae245e
Sende45 <ton-email@gmail.com>
2026-03-20 04:44:10 +0100

    ajout de la nouvelle modif des fichiers modifiers

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // On laisse Vite gérer les variables d'environnement via import.meta.env
-  // (C'est plus sûr car il ne prendra que celles qui commencent par VITE_)
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        // Optimisation pour séparer les grosses dépendances
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
   server: {
     port: 5173,
     strictPort: true,
